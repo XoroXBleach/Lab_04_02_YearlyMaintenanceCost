fa4b380b7c4d7bd884391d72bb8a9d5c337ef179
xoroxbleach <morinxander04@gmail.com>
2024-06-13 23:28:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double SummerCost = 1544.24;
+        double WinterCost = 1321.2;
+        double SpringCost = 1674.44;
+        double FallCost = 1764.33;
+        double total;
+        total = SummerCost + WinterCost + SpringCost + FallCost;
+        System.out.println("Your total is " + total + ". The summer maintenance cost is " + SummerCost  + ". The winter  is maintenance cost is " + WinterCost + ". The fall maintenance cost is " + FallCost + ". The spring maintenance cost is " + SpringCost);
+
+
+        }
+    }
